b36aebd625054a343b10e6f678f1dfd7cbf7639d
Andres2704 <62659416+Andres2704@users.noreply.github.com>
2020-08-19 11:44:07 -0300

    Add files via upload

--- tecdata/criarbd.py
+++ tecdata/criarbd.py
@@ -1,5 +1,12 @@
 import pymysql.cursors
 
+lon = [i for i in range(-180, 185, 5)]
+lat = []
+step = 87.5
+while step != -90:
+    lat.append(step)
+    step -= 2.5
+
 try:
     banco=pymysql.connect(
         host='us-cdbr-east-02.cleardb.com',
@@ -18,4 +25,11 @@ class criaBD():
         self.ano_final = ano_final
     
     def bd(self):
-        pass
+        for ano in range(self.ano_inicial, self.ano_final+1):
+            for i in lat:
+                for j in lon:
+                    with banco.cursor() as cursor:
+                        dbName = str(ano)+'_'+str(i)+'_'+str(j)
+                        sql = "CREATE TABLE `"+dbName+"`(dia int(11), hora0 int(11) not null, hora2 int(11) not null, hora4 int(11) not null, hora6 int(11) not null, hora8 int(11) not null, hora10 int(11) not null, hora12 int(11) not null, hora14 int(11) not null, hora16 int(11) not null, hora18 int(11) not null, hora20 int(11) not null, hora22 int(11) not null, hora24 int(11) not null, f107 double(10,2) NULL DEFAULT NULL)"
+                        cursor.execute(sql)
+        return 1
