de1f535edfa6387ec87992dc2ddf355b70419c0c
Andres Benoit <62659416+Andres2704@users.noreply.github.com>
2020-09-09 12:01:09 -0300

    Update api.py

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ try:
 except:
     print('error ao conectar com o banco de dados')
 
-#Função para obter o TEC específicado na barra de tarefa para determinado latxlon e ano
+#Função para obter o TEC específicado na barra de tarefa para determinado lat e ano
 def retornaTEC(ano,lat, dia_i, dia_f):
     tecs = []
     linha_i = dia_i*13
@@ -25,9 +25,42 @@ def retornaTEC(ano,lat, dia_i, dia_f):
             tecs = cursor.fetchall()
     except:
         print("SELECT * FROM `"+dbName+"` ORDER BY id ASC LIMIT "+str(linha_i-13)+","+str(linha_f)+"")
-        print('erro ao listar os TECs para o bando de dados:', dbName)
+        print('erro ao listar os TECs para a tabela:', dbName)
 
     return tecs
+
+def retornaF107(ano,dia_i,dia_f):
+    f107 = []
+    try:
+        with banco.cursor() as cursor:
+            cursor.execute("SELECT id, `"+str(ano)+"` FROM `f10.7` ORDER BY id ASC LIMIT "+str(dia_i-1)+","+str(dia_f)+"")
+            f107 = cursor.fetchall()
+    except:
+        print('erro ao listar os dados para a tabela: f10.7',)
+
+    return f107
+
+def retornaRSN(ano,dia_i,dia_f):
+    rsn = []
+    try:
+        with banco.cursor() as cursor:
+            cursor.execute("SELECT id, `"+str(ano)+"` FROM rsn ORDER BY id ASC LIMIT "+str(dia_i-1)+","+str(dia_f)+"")
+            rsn = cursor.fetchall()
+    except:
+        print('erro ao listar os os dados para a tabela: rsn',)
+
+    return rsn
+
+def retornaPhotonFlux(ano,dia_i,dia_f):
+    PhotonFlux = []
+    try:
+        with banco.cursor() as cursor:
+            cursor.execute("SELECT * FROM photonflux_"+str(ano)+" ORDER BY id ASC LIMIT "+str(dia_i-1)+","+str(dia_f)+"")
+            PhotonFlux = cursor.fetchall()
+    except:
+        print('erro ao listar os os dados para a tabela: PhotonFlux',)
+
+    return PhotonFlux
 
 #Início do código desenvolvido para a construção da API
 app = Flask(__name__)
@@ -35,7 +68,7 @@ app.config["DEBUG"] = True
 ###HOME---------------------------------------------------------------------
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>API para obter dados TEC</h1><p>Atualmente o Banco de dados está alimentado com dados dos anos de 2018 e 2019 - Atualizações estão sendo feitas</p><p><span style='font-style: italic'>Andres Benoit</span> - andres.benoit7@gmail.com</p>"
+    return "<h1>API para obter dados TEC - F10.7 - Sunspost e Photon Flux</h1><p>Atualmente o Banco de dados está alimentado com dados dos anos de 2017, 2018 e 2019 - Atualizações estão sendo feitas</p><p><span style='font-style: italic'>Andres Benoit</span> - andres.benoit7@gmail.com</p>"
 
 @app.route('/tec/', methods=['GET'])
 def obterTECdeBD():
@@ -45,7 +78,7 @@ def obterTECdeBD():
         dia_i = int(request.args['dia_i'])
         dia_f = int(request.args['dia_f'])
     else:
-        return "Error: Existem dados faltantes. Você deve especificar o ano, a latitude(lat) e de que dia a que dia quer obter os dados. Ex: /tec?ano=2019&lat=87.5&dia_i=-1&dia_f=10"
+        return "Error: Existem dados faltantes. Você deve especificar o ano, a latitude(lat) e de que dia a que dia quer obter os dados. Ex: /tec?ano=2019&lat=87.5&dia_i=1&dia_f=10"
     
     resultados = []
     for tec in retornaTEC(ano, lat, dia_i, dia_f):
@@ -53,7 +86,54 @@ def obterTECdeBD():
     
     return jsonify(resultados)
 
+@app.route('/f107/', methods=['GET'])
+def obterf107deBD():
+    if 'ano' in request.args:
+        ano = int(request.args['ano'])
+        dia_i = int(request.args['dia_i'])
+        dia_f = int(request.args['dia_f'])
+    else:
+        return "Error: Existem dados faltantes. Você deve especificar o ano e de que dia a que dia quer obter os dados. Ex: /f107?ano=2019&dia_i=1&dia_f=10"
+    
+    resultados = []
+    for f107 in retornaF107(ano, dia_i, dia_f):
+        resultados.append(f107)
+    
+    return jsonify(resultados)
+
+@app.route('/rsn/', methods=['GET'])
+def obterRSNdeBD():
+    if 'ano' in request.args:
+        ano = int(request.args['ano'])
+        dia_i = int(request.args['dia_i'])
+        dia_f = int(request.args['dia_f'])
+    else:
+        return "Error: Existem dados faltantes. Você deve especificar o ano e de que dia a que dia quer obter os dados. Ex: /rsn?ano=2019&dia_i=1&dia_f=10"
+    
+    resultados = []
+    for rsn in retornaRSN(ano, dia_i, dia_f):
+        resultados.append(rsn)
+    
+    return jsonify(resultados)
+
+@app.route('/photonflux/', methods=['GET'])
+def obterRSNdeBD():
+    if 'ano' in request.args:
+        ano = int(request.args['ano'])
+        dia_i = int(request.args['dia_i'])
+        dia_f = int(request.args['dia_f'])
+    else:
+        return "Error: Existem dados faltantes. Você deve especificar o ano e de que dia a que dia quer obter os dados. Ex: /rsn?ano=2019&dia_i=1&dia_f=10"
+    
+    resultados = []
+    for pf in retornaPhotonFlux(ano, dia_i, dia_f):
+        resultados.append(pf)
+    
+    return jsonify(resultados)
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
